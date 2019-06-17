dc15dd3bd97fac9d2ac90643b13aa46532811b61
Lenostatos <Lenostatos@gmx.de>
2019-06-17 17:21:31 +0200

    Add pictures to tree species tile set and construct a test tile set from it

--- VisualStudio_Project/MainFormsApp/Forms/game_form.Designer.vb
+++ VisualStudio_Project/MainFormsApp/Forms/game_form.Designer.vb
@@ -36,18 +36,20 @@ Partial Class game_form
         '
         'board_FlowLayoutPanel
         '
-        Me.board_FlowLayoutPanel.Location = New System.Drawing.Point(12, 12)
+        Me.board_FlowLayoutPanel.Location = New System.Drawing.Point(9, 10)
+        Me.board_FlowLayoutPanel.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.board_FlowLayoutPanel.Name = "board_FlowLayoutPanel"
-        Me.board_FlowLayoutPanel.Size = New System.Drawing.Size(746, 668)
+        Me.board_FlowLayoutPanel.Size = New System.Drawing.Size(560, 543)
         Me.board_FlowLayoutPanel.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(774, 23)
+        Me.Label1.Location = New System.Drawing.Point(580, 19)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(115, 17)
+        Me.Label1.Size = New System.Drawing.Size(91, 13)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Moving Player:"
         '
@@ -58,11 +60,12 @@ Partial Class game_form
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel1.Controls.Add(Me.Label2, 0, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.Label3, 1, 0)
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(777, 163)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(583, 132)
+        Me.TableLayoutPanel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 1
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(213, 49)
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(160, 40)
         Me.TableLayoutPanel1.TabIndex = 2
         '
         'Label2
@@ -70,9 +73,10 @@ Partial Class game_form
         Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(26, 16)
+        Me.Label2.Location = New System.Drawing.Point(19, 13)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(54, 17)
+        Me.Label2.Size = New System.Drawing.Size(42, 13)
         Me.Label2.TabIndex = 0
         Me.Label2.Text = "Player"
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -82,9 +86,10 @@ Partial Class game_form
         Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Right
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(135, 7)
+        Me.Label3.Location = New System.Drawing.Point(98, 7)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(75, 34)
+        Me.Label3.Size = New System.Drawing.Size(60, 26)
         Me.Label3.TabIndex = 1
         Me.Label3.Text = "# Tiles Collected"
         Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
@@ -92,9 +97,10 @@ Partial Class game_form
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(774, 51)
+        Me.Label4.Location = New System.Drawing.Point(580, 41)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(27, 17)
+        Me.Label4.Size = New System.Drawing.Size(22, 13)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "NA"
         '
@@ -102,9 +108,10 @@ Partial Class game_form
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(787, 455)
+        Me.Label5.Location = New System.Drawing.Point(590, 370)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(143, 17)
+        Me.Label5.Size = New System.Drawing.Size(113, 13)
         Me.Label5.TabIndex = 4
         Me.Label5.Text = "# Remaining Tiles:"
         '
@@ -112,26 +119,28 @@ Partial Class game_form
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(787, 540)
+        Me.Label6.Location = New System.Drawing.Point(590, 439)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(102, 17)
+        Me.Label6.Size = New System.Drawing.Size(80, 13)
         Me.Label6.TabIndex = 5
         Me.Label6.Text = "Time Played:"
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(787, 570)
+        Me.Label7.Location = New System.Drawing.Point(590, 463)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(27, 17)
+        Me.Label7.Size = New System.Drawing.Size(22, 13)
         Me.Label7.TabIndex = 6
         Me.Label7.Text = "NA"
         '
         'game_form
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1030, 707)
+        Me.ClientSize = New System.Drawing.Size(772, 574)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
@@ -139,6 +148,7 @@ Partial Class game_form
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.board_FlowLayoutPanel)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "game_form"
         Me.Text = "Memory"
         Me.TableLayoutPanel1.ResumeLayout(False)
---
+++ data/test_tile_set/fill_database.sql
@@ -0,0 +1,46 @@
+PRAGMA foreign_keys = ON;
+
+DELETE FROM "File_Info";
+DELETE FROM "File_Type";
+DELETE FROM "Item";
+
+INSERT INTO "Item" VALUES 
+	(1, 'Pine'),
+	(2, 'Oak'),
+	(3, 'Spruce'),
+	(4, 'Black Alder'),
+	(5, 'Birch')
+;
+
+INSERT INTO "File_Type" VALUES
+	(1, 'text'),
+	(2, 'png'),
+	(3, 'jpeg'),
+	(4, 'mp3')
+;
+
+INSERT INTO "File_Info" ("path", "id_Item", "id_File_Type") VALUES
+	('pine/english_name.txt', 1, 1),
+	('pine/latin_name.txt', 1, 1),
+	('pine/whole_tree.jpg', 1, 3),
+	('pine/leaves_and_cones.jpg', 1, 3),
+	('oak/english_name.txt', 2, 1),
+	('oak/latin_name.txt', 2, 1),
+	('oak/whole_tree.jpg', 2, 3),
+	('oak/leaves_and_acorns.jpg', 2, 3),
+	('spruce/english_name.txt', 3, 1),
+	('spruce/whole_tree.jpg', 3, 3),
+	('black_alder/english_name.txt', 4, 1),
+	('black_alder/latin_name.txt', 4, 1),
+	('black_alder/leaves_and_fruits.jpg', 4, 3),
+	('birch/latin_name.txt', 5, 1)
+;
+
+
+.mode column
+.headers on
+
+SELECT * FROM "Item";
+SELECT * FROM "File_Type";
+SELECT * FROM "File_Info";
+SELECT * FROM "File_Count";
